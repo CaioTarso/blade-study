c5e70094fcac61e8c56c61e90f4d35eb6d6a8f89
caiotarso <caiotarso7@gmail.com>
2025-12-23 20:08:37 -0300

    feat: criação da view home - componentização de layout - mudança na favicon - configuração do vite

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <x-slot:title>
+        Welcome
+    </x-slot:title>
+
+    <div class="max-w-2xl mx-auto">
+        <div class="card bg-base-100 shadow mt-8">
+            <div class="card-body">
+                <div>
+                    <h1 class="text-3xl font-bold">Bem vindo ao Twi... opa. Não é o Twitter!</h1>
+                    <p class="mt-4 text-base-content/60">Aqui você pode criar e compartilhar mensagens curtas, como o Twitter, mas não é o Twitter.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
