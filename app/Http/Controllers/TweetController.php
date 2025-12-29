2a58ccf7aa72fa202bdc8c885ad58c03077f6f12
caiotarso <caiotarso7@gmail.com>
2025-12-29 18:02:55 -0300

    feat: implementado Tweet model, migration, e  atualização do  TweetController e view home

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -2,30 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tweet;
 use Illuminate\Http\Request;
 
 class TweetController extends Controller
 {
     public function index() {
 
-        $tweets = [
-        [
-            'author' => 'Jane Doe',
-            'message' => 'Just deployed my first Laravel app! 🚀',
-            'time' => '5 minutes ago'
-        ],
-        [
-            'author' => 'John Smith',
-            'message' => 'Laravel makes web development fun again!',
-            'time' => '1 hour ago'
-        ],
-        [
-            'author' => 'Alice Johnson',
-            'message' => 'Working on something cool with Chirper...',
-            'time' => '3 hours ago'
-        ]
-    ];
-
+        $tweets = Tweet::with('user')->latest()->take(50)->get();
         return view('home', ['tweets' => $tweets]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,22 @@
-
 <x-layout>
     <x-slot:title>
-        Bem vindo(a)
+        Welcome
     </x-slot:title>
     <div class="max-w-2xl mx-auto">
-        @foreach ($tweets as $tweet)
+        @forelse ($tweets as $tweet)
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
                     <div>
-                        <div class="font-semibold">{{ $tweet['author'] }}</div>
-                        <div class="mt-1">{{ $tweet['message'] }}</div>
-                        <div class="text-sm text-gray-500 mt-2">{{ $tweet['time'] }}</div>
+                        <div class="font-semibold"> {{ $tweet->user ? $tweet->user->email : 'Anonymous' }}</div>
+                        <div class="mt-1">{{ $tweet->message }}</div>
+                        <div class="text-sm text-gray-500 mt-2">
+                            {{ $tweet->created_at->diffForHumans() }}
+                        </div>
                     </div>
                 </div>
             </div>
-        @endforeach
+        @empty
+            <p class="text-gray-500">Ainda não existe não tweets. Seja o primeiro!</p>
+        @endforelse
     </div>
 </x-layout>
